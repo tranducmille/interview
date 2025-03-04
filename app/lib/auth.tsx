7ac53eaa3242fe0994d7517eb3fc33777d8ab769
Tran Duc <tranducmille@gmail.com>
2025-03-03 23:20:34 -0600

    update

--- app/lib/auth.tsx
+++ app/lib/auth.tsx
@@ -6,8 +6,8 @@ export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
   pages: {
